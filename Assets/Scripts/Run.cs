b522ece173d8bc177f18f2bce476bc507db3bd05
abstractmachine <douglas@abstractmachine.net>
2014-05-21 22:43:37 +0200

    Demo complete

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Run : MonoBehaviour
+{
+	
+		Animator animator;
+	
+		void Start ()
+		{
+				animator = GetComponent<Animator> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+				float speed = animator.GetFloat ("speed");
+		
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						speed += 0.5f;
+				}
+				
+				speed = speed * 0.975f; // constantly slow down
+				
+				if (speed < 0.4f) {
+						speed = 0.0f;
+				}
+				
+				print ("speed = " + speed);
+				
+				animator.SetFloat ("speed", speed);
+	
+		}
+	
+	
+}
